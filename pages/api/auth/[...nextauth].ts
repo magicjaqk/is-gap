1d8cff1513e54816879787832e2e25fa3d8bdf32
Andrew Jacobs <magicjaqk@gmail.com>
2022-02-27 21:02:41 -0700

    Updated next-auth config and prisma schema

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import AppleProvider from "next-auth/providers/apple";
+import EmailProvider from "next-auth/providers/email";
 
 export default NextAuth({
   // Configure one or more authentication providers
